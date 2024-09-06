c64116626a7ce035bbf8f1e47d047c8d1202b4a0
Bytestream Technologies <coutinhosean1@gmail.com>
2024-09-06 08:08:39 -0700

    feat: contact us

--- pages/contact.js
+++ pages/contact.js
@@ -19,26 +19,27 @@ export default function Contact() {
                                         <div className="contact-info-item">
                                             <h5 className="title-two">USA Office</h5>
                                             <ul className="list-wrap">
-                                                <li>100 Wilshire Blvd, Suite 700 Santa <br /> Monica, CA 90401, USA</li>
-                                                <li>+1 (310) 620-8565</li>
-                                                <li>info@gmail.com</li>
+                                                <li>No.6 ,Room 11.Lucky Street, Kopje ,<br /> Harare, Zimbabwe</li>
+                                                <li>+263 775 349 085</li>
+                                              
+                                                <li>tyronepublishers@gmail.com</li>
                                             </ul>
                                         </div>
-                                        <div className="contact-info-item">
+                                        {/* <div className="contact-info-item">
                                             <h5 className="title-two">Australia Office</h5>
                                             <ul className="list-wrap">
-                                                <li>100 Wilshire Blvd, Suite 700 Santa <br /> Monica, CA 90401, USA</li>
+                                                <li>Harare Zimbabwe<br /> Monica, CA 90401, USA</li>
                                                 <li>+1 (310) 620-8565</li>
                                                 <li>info@gmail.com</li>
                                             </ul>
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </section>
                     <div className="contact-map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.332792000835!2d144.96011341744386!3d-37.805673299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d4c2b349649%3A0xb6899234e561db11!2sEnvato!5e0!3m2!1sen!2sbd!4v1685027435635!5m2!1sen!2sbd" allowFullScreen loading="lazy" />
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3798.121449441889!2d31.03519018073089!3d-17.832940739639806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1931a4fa3f3b45a5%3A0x14fece5c1e8bf33d!2s6%2C%2011%20Luck%20St%2C%20Harare%2C%20Zimbabwe!5e0!3m2!1sen!2szm!4v1725635274099!5m2!1sen!2szm" allowFullScreen loading="lazy" />
                     </div>
                 </div>
             </Layout>
